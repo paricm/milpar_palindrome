fb816a93b8d689f2011fd9e5794cc9af7ed1d96b
paricm <paric.milica@gmail.com>
2021-11-07 01:14:43 +0100

    Finish working again

--- test/milpar_palindrome_test.rb
+++ test/milpar_palindrome_test.rb
@@ -19,6 +19,14 @@ class MilparPalindromeTest < Minitest::Test
   def test_palindrome_with_punctuation
     assert "Madam, I'm Adam.".palindrome?
   end
+
+  def test_integer_non_palindrome
+    refute 12345.palindrome?
+  end
+
+  def test_integer_non_palindrome
+    assert 12321.palindrome?
+  end
   
 end
 
